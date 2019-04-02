bda1b45c7f2f858be1635db51a1d3d3fcea431ab
Anna-Petko <annapetko@icloud.com>
2019-04-02 16:55:38 +0200

    Input layout design

--- app/src/main/java/com/example/tdsapp/HomeActivity.java
+++ app/src/main/java/com/example/tdsapp/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.example.tdsapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private FloatingActionButton fabbtn;
 
 
 
@@ -17,6 +20,18 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("TDS App");
 
+        fabbtn = findViewById(R.id.fabadd);
+        fabbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddData();
+
+            }
+        });
+
+    }
+
+    private void AddData(){
 
     }
 }
