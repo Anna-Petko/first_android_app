4a3652b718c14299fe60c9fe2a7c68af030f3ce8
Anna-Petko <annapetko@icloud.com>
2019-04-02 15:19:40 +0200

    Adding design home activity and fab button

--- app/src/main/java/com/example/tdsapp/HomeActivity.java
+++ app/src/main/java/com/example/tdsapp/HomeActivity.java
@@ -2,12 +2,21 @@ package com.example.tdsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("TDS App");
+
+
     }
 }
